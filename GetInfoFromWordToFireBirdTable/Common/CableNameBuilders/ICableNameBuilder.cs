848621b9b0aa60eda6677ca4ac1e274f1d319777
delchew <delchew@mail.ru>
2021-02-09 18:12:57 +0300

    Deleted Cable name buildres (moved to library Cables)

--- GetInfoFromWordToFireBirdTable/Common/CableNameBuilders/ICableNameBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using GetInfoFromWordToFireBirdTable.CableEntityes;
-
-namespace GetInfoFromWordToFireBirdTable.Common.CableNameBuilders
-{
-    interface ICableNameBuilder<T> where T: Cable
-    {
-        string GetCableName(T cable);
-    }
-}
