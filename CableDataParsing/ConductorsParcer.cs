01ef8884b80f8a5f38f0d9305b5e3fd03726244f
delchew <delchew@gmail.com>
2021-02-20 00:50:57 +0300

    adding Kevv and Krsu parsers and conductors

---
+++ CableDataParsing/ConductorsParcer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CableDataParsing.TableEntityes;
+using FirebirdDatabaseProvider;
+
+namespace CableDataParsing
+{
+    public class ConductorsParcer : ICableDataParcer
+    {
+        private string _connectionString;
+        private FirebirdDBProvider _dBProvider;
+        private FirebirdDBTableProvider<ConductorPresenter> _condTableProvider;
+
+        public ConductorsParcer(string connectionString)
+        {
+            _connectionString = connectionString;
+            _dBProvider = new FirebirdDBProvider(_connectionString);
+            _condTableProvider = new FirebirdDBTableProvider<ConductorPresenter>(_dBProvider);
+        }
+
+        private ConductorPresenter[] _skabConductors = new ConductorPresenter[]
+        {
+              new ConductorPresenter { Title = "7х0.20 ММЛ", Class = 2, MetalId = 2, WiresCount = 7, WiresDiameter = 0.3m, ConductorDiameter = 0.6m, AreaInSqrMm = 0.2m },
+              new ConductorPresenter { Title = "7х0.26 ММЛ", Class = 2, MetalId = 2, WiresCount = 7, WiresDiameter = 0.26m, ConductorDiameter = 0.78m, AreaInSqrMm = 0.35m },
+              new ConductorPresenter { Title = "16х0.20 ММЛ", Class = 2, MetalId = 2, WiresCount = 16, WiresDiameter = 0.2m, ConductorDiameter = 0.94m, AreaInSqrMm = 0.5m },
+              new ConductorPresenter { Title = "14х0.26 ММЛ", Class = 2, MetalId = 2, WiresCount = 14, WiresDiameter = 0.26m, ConductorDiameter = 1.15m, AreaInSqrMm = 0.75m },
+              new ConductorPresenter { Title = "19х0.26 ММЛ", Class = 2, MetalId = 2, WiresCount = 19, WiresDiameter = 0.26m, ConductorDiameter = 1.3m, AreaInSqrMm = 1m },
+              new ConductorPresenter { Title = "12х0.40 ММЛ", Class = 2, MetalId = 2, WiresCount = 12, WiresDiameter = 0.4m, ConductorDiameter = 1.66m, AreaInSqrMm = 1.5m },
+              new ConductorPresenter { Title = "19х0.40 ММЛ", Class = 2, MetalId = 2, WiresCount = 19, WiresDiameter = 0.4m, ConductorDiameter = 2m, AreaInSqrMm = 2.5m }
+        };
+        
+        public event Action<int, int> ParseReport;
+
+        public int ParseDataToDatabase()
+        {
+            _dBProvider.OpenConnection();
+
+            int recordsCount = 0;
+            foreach(var conductor in _skabConductors)
+            {
+                _condTableProvider.AddItem(conductor);
+                recordsCount++;
+                ParseReport?.Invoke(_skabConductors.Length, recordsCount);
+            }
+
+            _dBProvider.CloseConnection();
+            return recordsCount;
+        }
+    }
+}
---
+++ CableDataParsing/Kevv-KerspBilletParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using CablesDatabaseEFCoreFirebird;
+using CablesDatabaseEFCoreFirebird.Entities;
+
+namespace CableDataParsing
+{
+    public class Kevv_KerspBilletParser : ICableDataParcer
+    {
+        public event Action<int, int> ParseReport;
+        public int ParseDataToDatabase()
+        {
+            int recordsCount = 0;
+            var conductorsIdList = new List<int> { 1, 3, 5, 7, 9 }; // заполнить правильно!
+            var cableShortnames = new List<int> { 3, 4 }; //3 - КЭВЭВ, 4 - КЭРсЭ
+
+            var cableBillet = new InsulatedBillet
+            {
+                OperatingVoltageId = 4
+            };
+            decimal minThickness;
+            using (var dbContext = new CablesContext())
+            {
+                foreach (var shortNameId in cableShortnames)
+                {
+                    cableBillet.CableShortNameId = shortNameId;
+                    cableBillet.PolymerGroupId = shortNameId == 3 ? 6 : 3; //Для КЭВЭВ - пвх LS, для КЭРсЭ - резина
+
+                }
+                foreach (var condId in conductorsIdList)
+                {
+                    cableBillet.ConductorId = condId;
+                    cableBillet.Diameter = GetDiameter(voltageId, condId, polymerId);
+                    minThickness = GetMinInsThickness(voltageId, polymerId);
+                    cableBillet.MinThickness = minThickness;
+                    cableBillet.NominalThickness = minThickness + 0.1m;
+
+                    dbContext.InsulatedBillets.Add(cableBillet);
+
+                    recordsCount++;
+                }
+
+                dbContext.SaveChanges();
+            }
+            return recordsCount;
+        }
+
+        private decimal GetMinInsThickness(int voltageId, int polymerGroupId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private decimal GetDiameter(int voltageId, int condId, int polymerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
