ea1c2ae0e976127b38b04c0438febedf1abacafa
delchew <delchew@mail.ru>
2021-03-05 17:52:36 +0300

    Start to develop Kpsvev parser. Whant to use abstract class instead interface in parcers entities

---
+++ CableDataParsing/CableParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using CableDataParsing.MSWordTableParsers;
+using CablesDatabaseEFCoreFirebird;
+using CablesDatabaseEFCoreFirebird.Entities;
+
+namespace CableDataParsing
+{
+    public abstract class CableParser : IDisposable
+    {
+        public event Action<int, int> ParseReport;
+        protected WordTableParser _wordTableParser;
+        protected FileInfo _mSWordFile;
+        protected CablesContext _dbContext;
+        protected int cablePropertiesCount = Enum.GetNames(typeof(Cables.Common.CableProperty)).Count();
+
+
+        public CableParser (string connectionString, FileInfo mSWordFile)
+        {
+            _mSWordFile = mSWordFile;
+            _dbContext = new CablesContext(connectionString);
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
+
+        public abstract string ParceDataToDatabase();
+
+        protected abstract string GetCableTitle(Cable cable, params object[] cableParametres);
+
+        protected void AddCablePropertiesToDBContext(Cable cable, Cables.Common.CableProperty cableProps)
+        {
+            if (_dbContext == null)
+                throw new NullReferenceException("Объект для работы с базой даных не инициализирован!");
+
+            var intProp = 0b_0000000001;
+            var cablePropertiesList = _dbContext.CableProperties.ToList();
+
+            for (int i = 0; i < cablePropertiesCount; i++)
+            {
+                var Prop = (Cables.Common.CableProperty)intProp;
+
+                if ((cableProps & Prop) == Prop)
+                {
+                    var propertyObj = cablePropertiesList.Where(p => p.BitNumber == (int)Prop).Single();
+                    _dbContext.ListCableProperties.Add(new ListCableProperties { Property = propertyObj, Cable = cable });
+                }
+                intProp <<= 1;
+            }
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ CableDataParsing/KpsvevParser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CableDataParsing.MSWordTableParsers;
+using CablesDatabaseEFCoreFirebird;
+using CablesDatabaseEFCoreFirebird.Entities;
+using Microsoft.EntityFrameworkCore;
+using WordObj = Microsoft.Office.Interop.Word;
+
+namespace CableDataParsing
+{
+    public class KpsvevParser : CableParser
+    {
+        private StringBuilder _nameBuilder = new StringBuilder();
+
+        public KpsvevParser(string connectionString, FileInfo mSWordFile) : base(connectionString, mSWordFile)
+        { }
+
+        public int ParseDataToDatabase()
+        {
+            int recordsCount = 0;
+
+            var app = new WordObj.Application { Visible = false };
+            object fileName = _mSWordFile.FullName;
+
+            try
+            {
+                app.Documents.Open(ref fileName);
+                var document = app.ActiveDocument;
+                var tables = document.Tables;
+                if(tables.Count > 0)
+                {
+                    _wordTableParser = new WordTableParser
+                    {
+                        
+                    };
+                    List<TableCellData> tableData;
+
+
+                }
+                else
+                    throw new Exception("Отсутствуют таблицы для парсинга в указанном Word файле!");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                app.Quit();
+            }
+
+            return recordsCount;
+        }
+
+        public int ParseBillets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ParceDataToDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetCableTitle(Cable cable, params object[] cableParametres)  //TODO
+        {
+            if (cableParametres != null)
+                if (cableParametres[0] is Cables.Common.CableProperty cableProps)
+                {
+                    _nameBuilder.Clear();
+                    _nameBuilder.Append("КПСВ");
+                    if ((cableProps & Cables.Common.CableProperty.HasFoilShield) == Cables.Common.CableProperty.HasFoilShield)
+                        _nameBuilder.Append("Э");
+                    string namePart;
+                    switch (cable.CoverPolymerGroup.Title)
+                    {
+                        case "PVC": namePart = "В";
+                            break;
+                        case "PVC LS": namePart = "Внг(А)-LS";
+                            break;
+                    }
+
+
+                    return _nameBuilder.ToString();
+                }
+            throw new Exception("В метод не передан параметр Cables.Common.CableProperty или параметр не соответствует этому типу данынх!");
+        }
+    }
+}
