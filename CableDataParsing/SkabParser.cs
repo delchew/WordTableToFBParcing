c456fa8877dc8a37856169c95a4a656cc350c5bd
delchew <delchew@mail.ru>
2021-04-12 18:16:11 +0300

    Continue to develop skab parsr with EF core

--- CableDataParsing/SkabParser.cs
+++ CableDataParsing/SkabParser.cs
@@ -25,22 +25,22 @@ namespace CableDataParsing
             var voltage250 = _dbContext.OperatingVoltages.Find(2);
             var voltage660 = _dbContext.OperatingVoltages.Find(3);
 
-            var skabModifycationsList = new List<(bool HasWaterblockStripe, bool HasFilling, bool HasBraidShield)>
-                    {
-                        (false, true, true),
-                        (true, true, true),
-                        (false, false, true),
-                        (true, false, true),
-                        (false, true, false),
-                        (true, true, false),
-                        (false, false, false),
-                        (true, false, false)
-                    };
+            //var skabModifycationsList = new List<(bool HasWaterblockStripe, bool HasFilling, bool HasBraidShield)>
+            //        {
+            //            (false, true, true),
+            //            (true, true, true),
+            //            (false, false, true),
+            //            (true, false, true),
+            //            (false, true, false),
+            //            (true, true, false),
+            //            (false, false, false),
+            //            (true, false, false)
+            //        };
             var skabPropertiesList = new List<CablePropertySet?>
             {
                 CablePropertySet.HasFilling | CablePropertySet.HasBraidShield,
                 CablePropertySet.HasWaterBlockStripe | CablePropertySet.HasFilling | CablePropertySet.HasBraidShield,
-                CablePropertySet.HasBraidShield,
+                CablePropertySet.HasBraidShield | CablePropertySet.HasFoilShield,
                 CablePropertySet.HasWaterBlockStripe | CablePropertySet.HasBraidShield,
                 CablePropertySet.HasFilling,
                 CablePropertySet.HasWaterBlockStripe | CablePropertySet.HasFilling,
@@ -50,13 +50,13 @@ namespace CableDataParsing
 
             var operatingVoltages = new List<OperatingVoltage> { voltage250, voltage660 }; // 1 - СКАБ 250, 2 - СКАБ 660 
 
-            var hasArmourList = new List<(bool hasArmourBraid, bool hasArmourTube)>
-                    {
-                        (hasArmourBraid: false, hasArmourTube: false),
-                        (hasArmourBraid: true, hasArmourTube: false),
-                        (hasArmourBraid: true, hasArmourTube: true)
-                    };
-            var skanArmorPropertiesList = new List<CablePropertySet?>
+            //var hasArmourList = new List<(bool hasArmourBraid, bool hasArmourTube)>
+            //        {
+            //            (hasArmourBraid: false, hasArmourTube: false),
+            //            (hasArmourBraid: true, hasArmourTube: false),
+            //            (hasArmourBraid: true, hasArmourTube: true)
+            //        };
+            var skabArmorPropertiesList = new List<CablePropertySet?>
             {
                 null,
                 CablePropertySet.HasArmourBraid,
@@ -69,14 +69,39 @@ namespace CableDataParsing
             var pair = _dbContext.TwistedElementTypes.Find(2);
             var triple = _dbContext.TwistedElementTypes.Find(3);
 
-            var twistTypesParamsList = new List<(TwistedElementType twistMode, CablePropertySet? hasIndividualFoilSHields, int dataStartRowIndex, int dataColumnsCount, int ColumnHeadersRowIndex)>
-                    {
-                        (single, null, 4, 13, 3),
-                        (pair, null, 11, 14, 10),
-                        (triple, null, 16, 14, 10),
-                        (pair, CablePropertySet.HasIndividualFoilShields, 22, 13, 21),
-                        (triple, CablePropertySet.HasIndividualFoilShields, 27, 13, 21)
-                    };
+            var fireLS = _dbContext.FireProtectionClasses.Find(8);
+            var fireHF = _dbContext.FireProtectionClasses.Find(13);
+            var fireFRLS = _dbContext.FireProtectionClasses.Find(18);
+            var fireFRHF = _dbContext.FireProtectionClasses.Find(23);
+            var fireCFRHF = _dbContext.FireProtectionClasses.Find(25);
+
+            var polymerLS = _dbContext.PolymerGroups.Find(6);
+            var polymerHF = _dbContext.PolymerGroups.Find(4);
+            var polymerRupper = _dbContext.PolymerGroups.Find(3);
+            var polymerPUR = _dbContext.PolymerGroups.Find(5);
+
+            var configurator = new TableParserConfigurator().SetDataRowsCount(5)
+                                                .SetDataStartColumnIndex(3)
+                                                .SetRowHeadersColumnIndex(2);
+
+
+            //var twistTypesParamsList = new List<(TwistedElementType twistMode, CablePropertySet? hasIndividualFoilSHields, int dataStartRowIndex, int dataColumnsCount, int ColumnHeadersRowIndex)>
+            //        {
+            //            (single, null, 4, 13, 3),
+            //            (pair, null, 11, 14, 10),
+            //            (triple, null, 16, 14, 10),
+            //            (pair, CablePropertySet.HasIndividualFoilShields, 22, 13, 21),
+            //            (triple, CablePropertySet.HasIndividualFoilShields, 27, 13, 21)
+            //        };
+
+            var twistParamsList = new List<(TwistedElementType twistMode, CablePropertySet? hasIndividualFoilSHields, TableParserConfigurator configurator)>
+            {
+                (single, null, new TableParserConfigurator(4, 3, 13, 5, 3, 2)),
+                (pair, null, new TableParserConfigurator(11, 3, 14, 5, 10, 2)),
+                (triple, null, new TableParserConfigurator(16, 3, 14, 5, 10, 2)),
+                (pair, CablePropertySet.HasIndividualFoilShields, new TableParserConfigurator(22, 3, 13, 5, 21, 2)),
+                (triple, CablePropertySet.HasIndividualFoilShields, new TableParserConfigurator(27, 3, 13, 5, 21, 2))
+            };
 
             var plasticInsMaterialParams = new List<(int fireProtectID, int insPolymerGroupId, int coverPolymerGroupId)>
                     {
@@ -87,24 +112,18 @@ namespace CableDataParsing
                         (18, 3, 6), (23, 3, 4), (25, 3, 5)
                     };
 
-            var exiParams = new List<bool> { false, true };
+            //var exiParams = new List<bool> { false, true };
+
+            var exiProperties = new List<CablePropertySet?> { null, CablePropertySet.SparkSafety };
 
             var billets = _dbContext.InsulatedBillets.Where(b => b.CableShortName.ShortName == "СКАБ")
                                                      .Include(b => b.Conductor)
                                                      .Include(b => b.PolymerGroup)
                                                      .ToList();
 
-            var listCableProperties = new ListCableProperties();
-            var listCableBillets = new ListCableBillets();
-
-            long recordId;
-
             var skabBoolPropertyesList = new List<(bool hasProp, CablePropertySet propType)>();
 
-            var configurator = new TableParserConfigurator().SetDataRowsCount(5)
-                                                            .SetDataStartColumnIndex(3)
-                                                            .SetRowHeadersColumnIndex(2);
-            _wordTableParser = new XceedWordTableParser(configurator);
+            _wordTableParser = new XceedWordTableParser();
             _wordTableParser.OpenWordDocument(_mSWordFile);
             var maxDiamTableCount = _wordTableParser.DocumentTablesCount / 2;
 
@@ -114,23 +133,24 @@ namespace CableDataParsing
 
             for (int i = 0; i < maxDiamTableCount; i++)
             {
-                foreach (var mod in skabModifycationsList)
+                foreach (var mod in skabPropertiesList)
                 {
                     foreach (var voltage in operatingVoltages)
                     {
                         foreach (var insType in insulationTypes)
                         {
-                            foreach (var armourType in hasArmourList)
+                            foreach (var armourType in skabArmorPropertiesList)
                             {
-                                foreach (var twistTypeParams in twistTypesParamsList)
+                                foreach (var twistTypeParams in twistParamsList)
                                 {
                                     if (twistTypeParams.hasIndividualFoilSHields.HasValue)
                                         cableProps |= twistTypeParams.hasIndividualFoilSHields.Value;
-                                    configurator.DataColumnsCount = twistTypeParams.dataColumnsCount;
-                                    configurator.ColumnHeadersRowIndex = twistTypeParams.ColumnHeadersRowIndex;
-                                    configurator.DataStartRowIndex = twistTypeParams.dataStartRowIndex;
+                                    if (mod.HasValue)
+                                        cableProps |= mod.Value;
+                                    if (armourType.HasValue)
+                                        cableProps |= armourType.Value;
 
-                                    var tableData = _wordTableParser.GetCableCellsCollection(i, configurator);
+                                    var tableData = _wordTableParser.GetCableCellsCollection(i, twistTypeParams.configurator);
 
                                     foreach (var tableCellData in tableData)
                                     {
@@ -141,8 +161,11 @@ namespace CableDataParsing
                                             var materialParams = insType == 0 ? plasticInsMaterialParams : rubberInsMaterialParams;
                                             foreach (var matParam in materialParams)
                                             {
-                                                foreach (var exiParam in exiParams)
+                                                foreach (var exiParam in exiProperties)
                                                 {
+                                                    if (exiParam.HasValue)
+                                                        cableProps |= exiParam.Value;
+
                                                     var billet = billets.Where(b => b.OperatingVoltage == voltage &&
                                                                                     b.PolymerGroupId == matParam.insPolymerGroupId &&
                                                                                     b.Conductor.AreaInSqrMm == conductorAreaInSqrMm)
@@ -155,45 +178,12 @@ namespace CableDataParsing
                                                     cable.MaxCoverDiameter = maxCoverDiameter;
                                                     cable.FireProtectionClassId = matParam.fireProtectID;
                                                     cable.CoverPolymerGroupId = matParam.coverPolymerGroupId;
-
-                                                    cable.HasIndividualFoilShields = twistTypeParams.hasIndividualFoilSHields;
-                                                    skabBoolPropertyesList.Add((cable.HasIndividualFoilShields, CablePropertySet.HasIndividualFoilShields));
-                                                    skabBoolPropertyesList.Add((cable.HasFoilShield, CablePropertySet.HasFoilShield));
-                                                    cable.HasBraidShield = mod.HasBraidShield;
-                                                    skabBoolPropertyesList.Add((cable.HasBraidShield, CablePropertySet.HasBraidShield));
-                                                    cable.HasFilling = mod.HasFilling;
-                                                    skabBoolPropertyesList.Add((cable.HasFilling, CablePropertySet.HasFilling));
-                                                    cable.HasArmourBraid = armourType.hasArmourBraid;
-                                                    skabBoolPropertyesList.Add((cable.HasArmourBraid, CablePropertySet.HasArmourBraid));
-                                                    cable.HasArmourTube = armourType.hasArmourTube;
-                                                    skabBoolPropertyesList.Add((cable.HasArmourTube, CablePropertySet.HasArmourTube));
-                                                    cable.HasWaterBlockStripe = mod.HasWaterblockStripe;
-                                                    skabBoolPropertyesList.Add((cable.HasWaterBlockStripe, CablePropertySet.HasWaterBlockStripe));
-                                                    cable.SparkSafety = exiParam;
-                                                    skabBoolPropertyesList.Add((cable.SparkSafety, CablePropertySet.SparkSafety));
                                                     cable.CoverColorId = (exiParam && armourType.hasArmourTube == false) ? 3 : 2;
                                                     cable.ClimaticModId = matParam.coverPolymerGroupId == 6 ? 3 : 7;  //3 - УХЛ, 7 - В
 
-                                                    cable.Title = cableTitleBuilder.GetCableTitle(cable, billet, cableProp);
+                                                    cable.Title = cableTitleBuilder.GetCableTitle(cable, billet, cableProps);
 
-                                                    recordId = _skabTableProvider.AddItem(cable);
-
-                                                    listCableBillets.CableId = recordId;
-                                                    listCableBillets.BilletId = billet.BilletId;
-                                                    _ListCableBilletsProvider.AddItem(listCableBillets);
-
-                                                    listCableProperties.CableId = recordId;
-
-                                                    foreach (var boolPropPair in skabBoolPropertyesList)
-                                                    {
-                                                        if (boolPropPair.hasProp)
-                                                        {
-                                                            listCableProperties.PropertyId = (long)boolPropPair.propType;
-                                                            _ListCablePropertiesProvider.AddItem(listCableProperties);
-                                                        }
-                                                    }
-
-                                                    skabBoolPropertyesList.Clear();
+                                                    ParseTableCellData(cable, tableCellData, new List<InsulatedBillet> { billet }, cableProps);
                                                     recordsCount++;
                                                 }
                                             }
